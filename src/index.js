a27eec03a31cf55a73d461d26a9596f18d5bc049
Serhii_Nikitin <Vverbatim123>
2018-10-31 10:18:22 +0200

    init webpack: js + inline css + html-template

---
+++ src/index.js
@@ -0,0 +1,5 @@
+// require('./style.css');
+__webpack_nonce__ = 'c29tZSBjb29sIHN0cmluZyB3aWxsIHBvcCB1cCAxMjM=';
+
+require('./style.css');
+require('../assets/template.html');
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+const ASSETS_PATH = './dist';
+
+module.exports = env => {
+  console.log(env);
+  return {
+    output: {
+      filename: '[name]-bundle.js',
+      path: path.resolve(__dirname, ASSETS_PATH)
+    },
+    entry: {
+      main: './src/index.js'
+    },
+    mode: 'development',
+    devServer: {
+      contentBase: 'dist',
+      overlay: true,
+      stats: {
+        colors: true
+      }
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        },
+
+        {
+          test: /\.html$/,
+          use: {}
+        }
+      ]
+    }
+  };
+};
